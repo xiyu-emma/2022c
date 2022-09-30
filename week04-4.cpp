4ed397d7e3b1b209d2b45aaaa991dda6a5a9db36
李希榆 <75963172+xiyu-emma@users.noreply.github.com>
2022-09-30 11:57:27 +0800

    week04

---
+++ week04-4.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    for(i=0; i<4; i++)
+    {
+        printf("印出%d\n", i);
+    }
+    for(i=1; i<=4; i++)
+    {
+        printf("人類的版本,印出%d\n", i);
+    }
+}
